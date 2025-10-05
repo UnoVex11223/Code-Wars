e767fa1b6a41430955002ce94b1dbd15a483a76c
Isaac Hughes <UnoVex11223@gmail.com>
2025-10-05 14:54:01 -0400

    Update Color_Ghost.py

--- Object_Oriented_Programming/Color_Ghost.py
+++ Object_Oriented_Programming/Color_Ghost.py
@@ -1,10 +1,8 @@
 import random
 
 class Ghost(object):
-    # Define the list as a Class Variable (standard for constants)
     COLORS = ["white", "yellow", "purple", "red"] 
 
     def __init__(self):
-        # Use 'pass' if the constructor is intentionally empty
         random_color = random.choice(Ghost.COLORS)
         self.color = random_color
